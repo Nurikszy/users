dbbf720d49980cb839ae8ff605720f33366bb51a
admin <admin@gmail.com>
2022-01-31 06:26:36 +0600

    commit

---
+++ users/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from . import models
+
+ADMIN = 1
+VIPClient = 2
+CLIENT = 3
+USER_TYPE = (
+    (ADMIN, 'ADMIN'),
+    (VIPClient, "VIP-Client"),
+    (CLIENT, 'CLIENT')
+)
+MALE = 1
+FEMALE = 2
+OTHER = 3
+GENDER_TYPE = (
+    (MALE, 'MALE'),
+    (FEMALE, "FEMALE"),
+    (OTHER, 'OTHER')
+)
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(required=True)
+    age = forms.IntegerField(required=True)
+    user_type = forms.ChoiceField(choices=USER_TYPE, required=True)
+    gender = forms.ChoiceField(choices=GENDER_TYPE, required=True)
+    culepassword = forms.CharField(max_length=90)
+
+    class Meta:
+        model = models.CustomUser
+        fields = (
+            "username",
+            "email",
+            "password1",
+            "password2",
+            "first_name",
+            "last_name",
+            "age",
+            "user_type",
+            "gender",
+            "culepassword"
+        )
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'username',
+            'id': 'hello'}
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "class": 'form-control',
+            'placeholder': 'password',
+            'id': 'hi',
+        }
+    ))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'email': 'from-control', 'placeholder': 'введите почту..', 'id': 'sup',}
+    ))
